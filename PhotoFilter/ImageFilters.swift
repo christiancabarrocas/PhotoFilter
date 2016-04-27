43ce9cac2deaee3045cc4656f469ff221938eeb2
Christian Cabarrocas <christiancabarrocas@gmail.com>
2016-04-27 21:43:21 +0200

    Updated

--- PhotoFilter/ImageFilters.swift
+++ PhotoFilter/ImageFilters.swift
@@ -20,7 +20,6 @@ public extension UIImageView {
     }
 
     public func blur(radius:Double) -> Filter {
-        
         return { image in
             let parameters = [kCIInputRadiusKey:radius,kCIInputImageKey:image]
             let filter = CIFilter(name: "CIGaussianBlur",withInputParameters:parameters)
