a21689a93a0a5d959a587a444df7d84451df829e
Christian Cabarrocas <christiancabarrocas@gmail.com>
2016-04-27 21:15:54 +0200

    Initial Commit

---
+++ PhotoFilter/PhotoFilter.h
@@ -0,0 +1,19 @@
+//
+//  PhotoFilter.h
+//  PhotoFilter
+//
+//  Created by Christian Cabarrocas on 27/04/16.
+//  Copyright © 2016 Wasabilabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PhotoFilter.
+FOUNDATION_EXPORT double PhotoFilterVersionNumber;
+
+//! Project version string for PhotoFilter.
+FOUNDATION_EXPORT const unsigned char PhotoFilterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PhotoFilter/PublicHeader.h>
+
+
